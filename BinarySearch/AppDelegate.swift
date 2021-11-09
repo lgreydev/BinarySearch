9f57277f60f4dc2b59e2235f170d2101e9b75b98
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-11-10 01:03:54 +0200

    feat: add constrain, hidden status bar

--- BinarySearch/AppDelegate.swift
+++ BinarySearch/AppDelegate.swift
@@ -10,10 +10,11 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
