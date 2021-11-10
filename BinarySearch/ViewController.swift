4025fe93b54b58c3c61a4dc310881028cf79ea39
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-11-11 00:54:44 +0200

    feat: dismiss keyboard, func create array

--- BinarySearch/ViewController.swift
+++ BinarySearch/ViewController.swift
@@ -10,28 +10,59 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    // MARK: Outlet
+    // MARK: - Outlet
     @IBOutlet weak var arrayNumbersTextField: UITextField!
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var indexLabel: UILabel!
     @IBOutlet weak var counterLabel: UILabel!
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-
+    // MARK: - Properties
     override var prefersStatusBarHidden: Bool {
         return true
     }
     
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // When user touch on screen
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tap)
+    }
     
-    // MARK: Action
+    
+    // MARK: - Private Methods
+    // Dismiss keyboard
+    @objc private func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    // Create array from 0 and to 'length'
+    private func createArray() -> [Int] {
+        var array: [Int] = []
+        guard let arrayLength = arrayNumbersTextField?.text, !arrayLength.isEmpty  else { fatalError() }
+        
+        if let length = Int(arrayLength) {
+            for number in 0..<length {
+                array.append(number)
+            }
+        }
+        return array
+    }
+    
+    // MARK: - Action
     @IBAction func start(_ sender: UIButton) {
+        let array: [Int] = createArray()
+        
+        
+        print(array)
     }
     
     @IBAction func restart(_ sender: UIButton) {
+        arrayNumbersTextField.text = ""
+        numberLabel.text = "0"
+        indexLabel.text = "0"
+        counterLabel.text = "0"
     }
 }
 
