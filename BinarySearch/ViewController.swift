d33caa93bff85f48242625b92335538e1f348c14
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-11-10 23:30:53 +0200

    feat: connected Outlet and Action to controller

--- BinarySearch/ViewController.swift
+++ BinarySearch/ViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    // MARK: Outlet
+    @IBOutlet weak var arrayNumbersTextField: UITextField!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var indexLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +25,13 @@ class ViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
-
+    
+    
+    // MARK: Action
+    @IBAction func start(_ sender: UIButton) {
+    }
+    
+    @IBAction func restart(_ sender: UIButton) {
+    }
 }
 
